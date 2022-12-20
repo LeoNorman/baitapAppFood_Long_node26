0224596ef9ad1abd135be1d31d93c20ba43e4e51
LeoNorman <kairyntvn222@gmail.com>
2022-12-20 18:08:16 +0700

    22.12.20 second times

--- src/routers/v1/index.js
+++ src/routers/v1/index.js
@@ -1,44 +1,29 @@
 // Routers V1
 const express = require("express");
-const userController = require("../../controllers/user.controller");
-const restaurantController = require("../../controllers/restaurant.controller");
 const authController = require("../../controllers/auth.controller");
-const orderController = require("../../controllers/order.controller")
-const orderDetailController = require("../../controllers/orderDetail.controller")
+const userRouter = require("./user.router");
+const restaurantRouter = require("./restaurant.router");
+const orderRouter = require("./order.router");
+const foodRouter = require("./food.router");
+const orderDetailRouter = require("./orderDetail.router");
 
 // path v1: /api/v1
 const v1 = express.Router();
 
 // Định nghĩa các routers cho users
-v1.get("/users", userController.getUsers());
-v1.get("/users/:id", userController.getUserByID());
-v1.post("/users", userController.createUser());
-v1.put("/users/:id", userController.updateUser());
-v1.delete("/users/:id", userController.deleteUser());
+v1.use("/", userRouter)
 
 // Định nghĩa các routers cho restaurants
-v1.get("/restaurantLikesByUserId/:userId", restaurantController.getRestaurantLikesbyUserId());
-v1.get("/getRestaurantLikesByRestaurantId/:restaurantId", restaurantController.getRestaurantLikesByRestaurantId());
-v1.get("/getRestaurantRatesByRestaurantId/:restaurantId", restaurantController.getRestaurantRatesbyRestaurantId())
-v1.get("/restaurantRatesByUserId/:userId", restaurantController.getRestaurantRatesByUserId());
-
-//like và unlike nhà hàng
-v1.post(
-  "/restaurants/:restaurantId/like",
-  restaurantController.likeRestaurant()
-);
-v1.delete("/restaurants/:restaurantId/unLike", restaurantController.unLikeRestaurant())
-
-// Đánh giá nhà hàng
-v1.post("/restaurants/:restaurantId/rate", restaurantController.rateRestaurant())
+v1.use("/", restaurantRouter)
 
 // Định nghĩa các routers cho orders
-v1.get("/orders", orderController.getOrders());
-v1.post("/orders", orderController.createdOrder());
+v1.use("/", orderRouter)
+
+// Định nghĩa các routers cho foods
+v1.use("/", foodRouter)
 
 // Định nghĩa các rounters cho orderDetail
-v1.get("/orderDetails", orderDetailController.getOrderDetails())
-v1.post("/orderDetails", orderDetailController.createdOrderDetails())
+v1.use("/", orderDetailRouter)
 
 // Định nghĩa các routers cho auth
 v1.post("/login", authController.login());
